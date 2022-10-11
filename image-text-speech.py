2a84b09c86210b0b18258674a499a692d6a0ea8c
Minerva Sarma <81676380+minerva-proj@users.noreply.github.com>
2022-10-11 20:20:47 +0100

    Add files via upload

---
+++ image-text-speech.py
@@ -0,0 +1,40 @@
+## Download tesseract engine from: https://osdn.net/projects/sfnet_tesseract-ocr-alt/downloads/tesseract-ocr-setup-3.02.02.exe/
+## Install following libraries:
+# pip install pytesseract
+# pip install opencv-python
+# pip install pillow
+# pip install gTTS
+# pip install pygame
+
+# Import liraries
+import pytesseract
+from pytesseract import Output
+import cv2
+import numpy as np
+from PIL import Image
+from gtts import gTTS
+import os
+from pygame import mixer
+
+
+#IMAGE TO TEXT
+# Path to the image
+imagePath = "D:\BKP\D Drive\Medikabazaar\Invoices\images\Original\OHI marketing.jpg"
+imgRead = np.array(Image.open(imagePath))
+
+# path to tesseract engine
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+getTheText = pytesseract.image_to_string(imgRead, config="--psm 6")
+print(getTheText)
+
+# TEXT TO SPEECH
+language = 'en'
+myobj = gTTS(text=getTheText, lang=language, slow=False)
+  
+# Saving the converted audio in a mp3 file named
+myobj.save(r"D:\BKP\D Drive\Medikabazaar\Invoices\images\Original\textToSpeech.mp3")
+  
+# Playing the converted file
+mixer.init()
+mixer.music.load(r"D:\BKP\D Drive\Medikabazaar\Invoices\images\Original\textToSpeech.mp3")
+mixer.music.play()
